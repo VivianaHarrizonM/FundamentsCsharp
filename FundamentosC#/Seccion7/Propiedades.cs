dc7466ade96e73eff07c3a6295fc4e59c3515475
Viviana Harrizon <viviana.harrizon@gmail.com>
2025-07-04 14:31:18 -0600

    Changes in properties

--- FundamentosC#/Seccion7/Propiedades.cs
+++ FundamentosC#/Seccion7/Propiedades.cs
@@ -8,36 +8,52 @@ namespace FundamentosC_.Seccion7
 {
     internal class Propiedades
     {
-        static void Main(string[] args)
-        {
-            //bool acelerar;
+        //static void Main(string[] args)
+        //{
+        //    //bool acelerar;
 
-            Automovil automovil1 = new Automovil();
+        //    Automovil automovil1 = new Automovil();
 
-            //Mostramos campo privado
-            Console.WriteLine("El color es: {0}", automovil1.Color);
+        //    //Mostramos campo privado
+        //    Console.WriteLine("El color es: {0}", automovil1.Color);
 
-            //Asignamos valor a un campo privado
-            automovil1.Combustible = "Diesel";
-            Console.WriteLine("El combustible usado es: {0}",automovil1.Combustible);
+        //    //Asignamos valor a un campo privado
+        //    automovil1.Combustible = "Diesel";
+        //    Console.WriteLine("El combustible usado es: " +
+        //        "{0}",automovil1.Combustible);
+
+        //    Console.WriteLine(automovil1.ToString());
+
            
-        }
+        //}
 
         public class Automovil
         {
-            private string color= "Azul", modelo, combustible;
-            private byte numPuertas, año;
-            private int ccMotor;
+            private string color= "Azul", modelo= "ZB10", combustible;
+            private string numPuertas = "4", año = "2000";
+            private int ccMotor = 1500;
 
+            private String asientos, colorTablero;
+            private bool camaraTrasera;
+
+            public Automovil()
+            {
+                asientos = "Piel";
+                colorTablero = "Beige";
+                camaraTrasera = false;
+            }
             public string Color
             {
-                get { return color; }
+                //get { return color; }
                 //set { color = value; };
+                get => color;
             }
             public string Combustible
             {
-                get { return combustible; }
-                set { combustible = value; }
+                //get { return combustible; }
+                get => combustible;
+                //set { combustible = value; }
+                set => combustible = value;
 
             }
             public bool Acelerar()
@@ -59,10 +75,21 @@ namespace FundamentosC_.Seccion7
                 velocidadPa++;
                 Console.WriteLine("Cambio de velocidad");
             }
+            //Metodo estatico
             public static void Prueba()
             {
                 Console.WriteLine("Soy un método estático");
             }
+
+            //INvalidando el metodo ToString
+            public override string ToString()
+            {
+                string mensaje;
+                mensaje = " Modelo: " + modelo + "\n Color: "+color + "\n Año: " + año +"\n Puertas: "+ numPuertas +"\n Tipo de combustible: "+ combustible + "\n Motor: " + ccMotor + "\n Asientos: " + asientos + "\n Color del tablero: " + colorTablero + "\n Camara Trasera: " + camaraTrasera;
+
+                return mensaje;
+            }
+
         }
     }
 }
